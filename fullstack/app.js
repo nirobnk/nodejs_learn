f5210cc1eed70b5b7a0e9d6b9d78ce2e209976a2
nirobnk <eg245158@engug.ruh.ac.lk>
2024-12-07 05:12:51 +0530

    day 9 commit

--- fullstack/app.js
+++ fullstack/app.js
@@ -3,15 +3,20 @@ let app = express();
 let dotenv = require("dotenv");
 dotenv.config();
 let port = process.env.PORT || 6700;
+let categoryRouter = require("./src/controller/CategoryRouter");
+let productRouter = require("./src/controller/ProductRouter");
+
+
 
 //default
 app.get("/", (req, res) => {
   res.send("hii from Express");
 });
 
-app.get("/test", (req, res) => {
-  res.send("hii from test");
-});
+
+
+app.use("/category", categoryRouter);
+app.use("/products",productRouter);
 
 app.listen(port, (err) => {
   if (err) throw err;
