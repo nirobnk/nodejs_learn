fe06e1d4f3fd55e208c77a6df751ae00880f853e
nirobnk <eg245158@engug.ruh.ac.lk>
2024-12-08 06:18:20 +0530

    today learn little bit about ejs file

--- fullstack/app.js
+++ fullstack/app.js
@@ -2,21 +2,30 @@ let express = require("express");
 let app = express();
 let dotenv = require("dotenv");
 dotenv.config();
+let morgan = require("morgan");
+let fs = require("fs");
 let port = process.env.PORT || 6700;
 let categoryRouter = require("./src/controller/CategoryRouter");
 let productRouter = require("./src/controller/ProductRouter");
 
+//middleware
+app.use(morgan("common", { stream: fs.createWriteStream("./app.log") }));
 
+//static file path
+app.use(express.static(__dirname + "/public"));
+//html file path
+app.set("views", "./src/views");
+//view engine
+app.set("view engine", "ejs");
 
 //default
 app.get("/", (req, res) => {
-  res.send("hii from Express");
+  //res.send("hii from Express");
+  res.render("index",{title:"Home page"});
 });
 
-
-
 app.use("/category", categoryRouter);
-app.use("/products",productRouter);
+app.use("/products", productRouter);
 
 app.listen(port, (err) => {
   if (err) throw err;
--- inbuilt/fs.js
+++ inbuilt/fs.js
@@ -1,12 +1,40 @@
 let fs = require("fs");
-let u = "my name is dimuthu";
+let u = "my name is Buddhika Niroshan Kumarasinghe";
 
-fs.writeFile("mycode.txt", u, (err) => {
-  if (err) throw err;
-  console.log("Task done");
-});
+// fs.writeFileSync("mycode.txt", u, (err) => {
+//   if (err) throw err;
+//   console.log("Task done");
+// });
 
-fs.readFile("mycode.txt", "utf-8", (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
+// fs.readFile("mycode.txt", "utf-8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+//const fs = require('fs');
+
+// console.log('Start');
+// fs.readFileSync('mycode.txt', 'utf8', (err, data) => { // Asynchronous file read
+//   if (err) throw err;
+//   console.log(data);
+// });
+// console.log('End');
+
+//using synchronous method with error handling
+
+try {
+  fs.writeFileSync("mycode.txt", u);
+  console.log("file has been written successfully!!");
+} catch (err) {
+  console.error("error occured while writing the file", err);
+}
+
+console.log("dimuthu");
+
+try {
+  const data = fs.readFileSync("mycode.txt", "utf-8");
+  console.log("Readfile successfully!!");
+  console.log("content:", data);
+} catch (err) {
+  console.error("error occured while reading the file", err);
+}
